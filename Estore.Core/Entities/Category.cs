af5f4803d291ee8034fe42e44ad22dbdaa171832
solmazkadir <abdlkadirsolmaz@gmail.com>
2023-05-01 23:06:36 +0300

    Add project files.

---
+++ Estore.Core/Entities/Category.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Estore.Core.Entities
+{
+    public class Category : IEntity
+    {
+        public int Id { get; set; }
+        [Display(Name = "Ad")]
+        public string Name { get; set; }
+        [Display(Name = "Açıklama")]
+        public string? Description { get; set; }
+        [Display(Name = "Resim")]
+        public string? Image { get; set; }
+        [Display(Name = "Durum")]
+        public bool IsActive { get; set; }
+        [Display(Name = "Üst Menüde Göster")]
+        public bool IsTopMenu { get; set; }
+        [Display(Name = "Üst Kategori")]
+        public int ParentId { get; set; }
+        [Display(Name = "Sıra No")]
+        public int OrderNo { get; set; }
+        [Display(Name = "Eklenme Tarihi"),ScaffoldColumn(false)]
+        public DateTime? CreateDate { get; set; } = DateTime.Now;
+
+    }
+}
